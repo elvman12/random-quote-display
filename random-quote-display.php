834d735621765e784a96aaafcec09282115ca71a
elvman12 <elvis.sherman@gmail.com>
2016-09-25 20:15:20 -0600

    9/24 custom post type

--- random-quote-display.php
+++ random-quote-display.php
@@ -11,8 +11,25 @@ License URI: https://www.gnu.org/licenses/gpl-2.0.html
 */
 
 function ctd_famous_quotes() {
-	register_post_type( 'quote',
-	array (
+	
+	$label = array(
+		'name'               => 'Quotes',
+		'singular_name'      => 'Quote',
+		'menu_name'          => 'Quote',
+		'name_admin_bar'     => 'Quote',
+		'add_new'            => 'Add New',
+		'add_new_item'       => 'Add New Quote',
+		'new_item'           => 'New Quote',
+		'edit_item'          => 'Edit Quote',
+		'view_item'          => 'View Quote',
+		'all_items'          => 'All Quotes',
+		'search_items'       => 'Search Quotes',
+		'parent_item_colon'  => 'Parent Quotes:',
+		'not_found'          => 'No Quotes found.',
+		'not_found_in_trash' => 'No Quotes found in Trash.'
+	);
+	
+	$args = array( 
 		'public' => true,
 		'label' => 'Quotes',
 		'publicly_queryable' => false,
@@ -20,7 +37,9 @@ function ctd_famous_quotes() {
 		'menu_position' => 5,
 		'menu_icon' => 'dashicons-format-quote',
 		'supports' => array( 'title', 'editor', 'author', 'thumbnail', 'excerpt' )
-	));
+	);
+	
+	register_post_type( 'quote', $args );
 }
 add_action( 'init', 'ctd_famous_quotes' );
 
