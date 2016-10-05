dc8063044635a0011a767d1ddeec5831ca795dda
elvman12 <elvis.sherman@gmail.com>
2016-10-05 08:56:26 -0600

    10/5 custom post type changes

---
+++ random-quote-display2.php
@@ -0,0 +1,195 @@
+<?php
+/*
+Plugin Name: Random Quote Display
+Plugin URI:  Not applicable
+Description: Custom Post Type to record quotes and credit names.
+Version:     1.0
+Author:      Elvis Sherman
+Author URI:  http://wwww.clicktimedesign.com/
+License:     Pretty much free, enjoy.
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+*/
+
+function ctd_famous_quotes() {
+	
+	$labels = array(
+		'name'               => 'Quotes',
+		'singular_name'      => 'Quote',
+		'menu_name'          => 'Quote',
+		'name_admin_bar'     => 'Quote',
+		'add_new'            => 'Add New',
+		'add_new_item'       => 'Add New Quote',
+		'new_item'           => 'New Quote',
+		'edit_item'          => 'Edit Quote',
+		'view_item'          => 'View Quote',
+		'all_items'          => 'All Quotes',
+		'search_items'       => 'Search Quotes',
+		'parent_item_colon'  => 'Parent Quotes:',
+		'not_found'          => 'No Quotes found.',
+		'not_found_in_trash' => 'No Quotes found in Trash.'
+	);
+	
+	$args = array( 
+		'public' => true,
+		'hierarchical' => false,
+		'labels' => $labels,
+		'publicly_queryable' => false,
+		'rewrite' => array( 'slug' => 'quote' ),
+		'menu_position' => 5,
+		'show_ui' => true,
+		'show_in_admin_bar' => true,
+		'menu_icon' => 'dashicons-format-quote',
+		'supports' => array ( 'title' )
+	);
+	
+	register_post_type( 'quote', $args );
+}
+add_action( 'init', 'ctd_famous_quotes' );
+
+// Change the place holder text for entering a new famous quote
+function ctd_change_title_text( $title ){
+     $screen = get_current_screen();
+ 
+     if  ( 'quote' == $screen->post_type ) {
+          $title = 'Enter a Famous Quote';
+     }
+ 
+     return $title;
+}
+ 
+add_filter( 'enter_title_here', 'ctd_change_title_text' );
+
+///////////////////////////////
+// Add Quote Credit (Author) //
+///////////////////////////////
+
+// Meta Box Markup for the HTML and some CSS
+function custom_meta_box_markup($object)
+{
+    wp_nonce_field(basename(__FILE__), "meta-box-nonce");
+
+    ?>
+    	<style>
+			#ctd-custom-post-quote input[type=text] {
+				padding: 3px 8px;
+				font-size: 1.7em;
+				line-height: 100%;
+				height: 1.7em;
+				width: 100%;
+				outline: 0;
+				margin: 0 0 3px;
+				background-color: #fff;	
+			}
+			
+			#normal-sortables {width: 50%;}
+			
+			#demo-meta-box h2 span {
+				font-size: 20px;
+				font-weight: 400;
+				margin: 0;
+				padding: 9px 15px 4px 0;
+				line-height: 29px;	
+			}
+		</style>
+    
+        <div id="ctd-custom-post-quote">
+            <!--<label for="meta-box-text">Quote Credit</label>-->
+            <input name="meta-box-text" type="text" value="<?php echo get_post_meta($object->ID, "meta-box-text", true); ?>">
+
+            <br>
+
+            <!--<label for="meta-box-dropdown">Dropdown</label>
+            <select name="meta-box-dropdown">-->
+                <?php 
+                    //$option_values = array(1, 2, 3);
+
+                    //foreach($option_values as $key => $value) 
+                    //{
+                        //if($value == get_post_meta($object->ID, "meta-box-dropdown", true))
+                        //{
+                            ?>
+                                <!--<option selected><?php //echo $value; ?></option>
+                            <?php    
+                        //}
+                        //else
+                        //{
+                            ?>
+                                <option><?php //echo $value; ?></option>-->
+                            <?php
+                        //}
+                    //}
+                ?>
+            <!--</select>-->
+
+            <br>
+
+            <!--<label for="meta-box-checkbox">Check Box</label>-->
+            <?php
+                //$checkbox_value = get_post_meta($object->ID, "meta-box-checkbox", true);
+
+                //if($checkbox_value == "")
+                //{
+                    ?>
+                        <!--<input name="meta-box-checkbox" type="checkbox" value="true">
+                    <?php
+                //}
+                //else if($checkbox_value == "true")
+                //{
+                    ?>  
+                        <input name="meta-box-checkbox" type="checkbox" value="true" checked>-->
+                    <?php
+                //}
+            ?>
+        </div>
+    <?php  
+}
+
+// Add the Box Itself
+function add_custom_meta_box()
+{
+    add_meta_box("demo-meta-box", "Quote Credit (Author)", "custom_meta_box_markup", "quote", "side", "high", null);
+}
+
+add_action("add_meta_boxes", "add_custom_meta_box");
+
+// Add Data to Dbase
+function save_custom_meta_box($post_id, $post, $update)
+{
+    if (!isset($_POST["meta-box-nonce"]) || !wp_verify_nonce($_POST["meta-box-nonce"], basename(__FILE__)))
+        return $post_id;
+
+    if(!current_user_can("edit_post", $post_id))
+        return $post_id;
+
+    if(defined("DOING_AUTOSAVE") && DOING_AUTOSAVE)
+        return $post_id;
+
+    $slug = "post";
+    if($slug != $post->post_type)
+        return $post_id;
+
+    $meta_box_text_value = "";
+    $meta_box_dropdown_value = "";
+    $meta_box_checkbox_value = "";
+
+    if(isset($_POST["meta-box-text"]))
+    {
+        $meta_box_text_value = $_POST["meta-box-text"];
+    }   
+    update_post_meta($post_id, "meta-box-text", $meta_box_text_value);
+
+    if(isset($_POST["meta-box-dropdown"]))
+    {
+        $meta_box_dropdown_value = $_POST["meta-box-dropdown"];
+    }   
+    update_post_meta($post_id, "meta-box-dropdown", $meta_box_dropdown_value);
+
+    if(isset($_POST["meta-box-checkbox"]))
+    {
+        $meta_box_checkbox_value = $_POST["meta-box-checkbox"];
+    }   
+    update_post_meta($post_id, "meta-box-checkbox", $meta_box_checkbox_value);
+}
+
+add_action("save_post", "save_custom_meta_box", 10, 3);
+?>
