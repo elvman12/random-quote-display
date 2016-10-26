0c853a7228a86a7e8d1c4a2a7da26599ea77547e
elvman12 <elvis.sherman@gmail.com>
2016-10-26 10:06:06 -0600

    dsfgsdfgsdg

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,5 @@
 jQuery(document).ready(function($){
     //alert("Hello");
 	console.log ('Elvis Sherman TESTING');
-	$( '<p style="color:red; font-size:30px;">This is a new paragraph, hope it shows up...that would be nice.</p><br><br>' ).insertAfter( '#titlediv' ); 
+	//$( '<p style="color:red; font-size:30px;">This is a new paragraph, hope it shows up...that would be nice.</p><br><br>' ).insertAfter( '#titlediv' ); 
 });
