dc5f79d279c25db9b8fa706d41f1e53435914712
elvman12 <elvis.sherman@gmail.com>
2016-10-26 21:09:48 -0600

    10/26 Plugin Work

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,7 @@
 jQuery(document).ready(function($){
     //alert("Hello");
-	console.log ('Elvis Sherman TESTING');
-	//$( '<p style="font-size:30px;">This is a new paragraph, hope it shows up...that would be nice.</p><br><br>' ).insertAfter( '#titlediv' ); 
+	console.log ('ESherman TESTING');
+	$( '<p style="font-size:13px;">Enter the name of the author or person credited with this quote.</p>' ).insertAfter( '#authorinput' );
+	
+	$( '<p style="font-size:13px;">Enter the quote, do NOT add quotation marks.</p>' ).insertAfter( '#quoteinput' ); 
 });
